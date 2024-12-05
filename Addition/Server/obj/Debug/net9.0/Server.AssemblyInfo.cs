921e775c186cd6f9256336f4e66908e15bd4d990
Scott S. Washburn <34353731+swash17@users.noreply.github.com>
2024-12-05 09:39:40 -0500

    GitIngore file revisions

--- Addition/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Addition/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7217683d4b86575a94bbbc90002ba469ce00ea21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
